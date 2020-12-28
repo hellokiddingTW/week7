381a3b48ab7a53d9463983da4bb09c29bd3f149a
Nate Yeh <a498390344@gmail.com>
2020-12-28 15:07:50 +0800

    Update 2020-12-28T07:07:48.150Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,22 @@
 "use strict";
 
+var _lax = _interopRequireDefault(require("lax.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+window.onload = function () {
+  _lax["default"].setup(); // init
+
+
+  var updateLax = function updateLax() {
+    _lax["default"].update(window.scrollY);
+
+    window.requestAnimationFrame(updateLax);
+  };
+
+  window.requestAnimationFrame(updateLax);
+};
+
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
