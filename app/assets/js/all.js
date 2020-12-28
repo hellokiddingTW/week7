f829f49feaffa8177a42ddfd411b0a8435aa8d87
Nate Yeh <a498390344@gmail.com>
2020-12-28 15:11:06 +0800

    first commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,17 @@
+import lax from 'lax.js';
+
+
+window.onload = function() {
+  lax.setup() // init
+
+  const updateLax = () => {
+    lax.update(window.scrollY)
+    window.requestAnimationFrame(updateLax)
+  }
+
+  window.requestAnimationFrame(updateLax)
+}
+
 console.log('Hello!');
 
 $(document).ready(() => {
