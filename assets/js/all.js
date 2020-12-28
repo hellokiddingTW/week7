6ced2f2bf87ace27a1ea2780e20e971703a11f92
Nate Yeh <a498390344@gmail.com>
2020-12-28 20:48:50 +0800

    Update 2020-12-28T12:48:47.722Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,22 +1,22 @@
 "use strict";
 
-var _lax = _interopRequireDefault(require("lax.js"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-window.onload = function () {
-  _lax["default"].setup(); // init
-
-
-  var updateLax = function updateLax() {
-    _lax["default"].update(window.scrollY);
-
-    window.requestAnimationFrame(updateLax);
-  };
-
-  window.requestAnimationFrame(updateLax);
-};
-
+// import lax from 'lax.js';
+// window.onload = function () {
+//   lax.init()
+//   // Add a driver that we use to control our animations
+//   lax.addDriver('scrollY', function () {
+//     return window.scrollY
+//   })
+//   // Add animation bindings to elements
+//   lax.addElements('.selector', {
+//     scrollY: {
+//       translateX: [
+//         ["elInY", "elCenterY", "elOutY"],
+//         [0, 'screenWidth/2', 'screenWidth'],
+//       ]
+//     }
+//   })
+// }
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
